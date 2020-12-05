a14bcbd6b5c019596320e8179e0727c5c889fb06
VladimirGarciaM <vladimirl.garciam@gmail.com>
2020-12-04 19:00:49 -0500

    front usuario autor  y cliente

---
+++ src/app/disposicion/disposicion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disposicion',
+  templateUrl: './disposicion.component.html',
+  styleUrls: ['./disposicion.component.css']
+})
+export class DisposicionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
